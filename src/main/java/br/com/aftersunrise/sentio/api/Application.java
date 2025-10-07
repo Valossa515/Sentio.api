134fb70f58a059ac14782b7cfb27be108eda0ada
Valossa515 <fe.mmo515@gmail.com>
2025-10-07 13:04:46 -0300

    Criando o controller

--- src/main/java/br/com/aftersunrise/sentio/api/Application.java
+++ src/main/java/br/com/aftersunrise/sentio/api/Application.java
@@ -2,8 +2,14 @@ package br.com.aftersunrise.sentio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.aftersunrise.sentio")
+@EnableJpaRepositories(basePackages = "br.com.aftersunrise.sentio.infrastructure.repositories")
+@EntityScan(basePackages = "br.com.aftersunrise.sentio.domain.models")
 public class Application {
 
 	public static void main(String[] args) {
