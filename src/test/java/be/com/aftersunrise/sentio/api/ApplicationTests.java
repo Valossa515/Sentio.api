d32e0e3d403aff5ed700293a1e3fe755056e6e58
Valossa515 <fe.mmo515@gmail.com>
2025-10-07 10:11:51 -0300

    Initial commit

---
+++ src/test/java/be/com/aftersunrise/sentio/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.com.aftersunrise.sentio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
