0925a24ffff03be6fc1f9a4bb137b6b51e3f1c2c
Valossa515 <fe.mmo515@gmail.com>
2025-10-07 10:19:48 -0300

    Estrutura inicial do projeto usando DDD

--- src/main/java/br/com/aftersunrise/sentio/api/Application.java
+++ src/main/java/br/com/aftersunrise/sentio/api/Application.java
@@ -1,4 +1,4 @@
-package be.com.aftersunrise.sentio.api;
+package br.com.aftersunrise.sentio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
